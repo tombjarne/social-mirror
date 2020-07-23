eb7d48ea757e77d537da462b4b73ef77c6db08d5
tombjarne <bjarne.seidel@icloud.com>
2020-07-23 15:33:25 +0200

    Update page

--- src/components/pages/recommendations.js
+++ src/components/pages/recommendations.js
@@ -9,6 +9,8 @@ import friends from "../../assets/img/user-alt-solid.png";
 import facebook from "../../assets/img/facebook.png";
 import meetup from "../../assets/img/Meetup_Logo.png";
 import mightynetworks from "../../assets/img/mightynetworks.png";
+import socialgroups from "../../assets/img/facebook-2-map.png";
+import newcomer from "../../assets/img/newcomer.png";
 
 class Recommendations extends React.Component {
 
@@ -49,7 +51,7 @@ class Recommendations extends React.Component {
                     <div id="hero-cta">
                         <div>
                             <h1>Empfehlungen</h1>
-                            <p className="slim-text">Welche Empfehlungen und Tipps wir dir geben können ein sozialeres Leben auf Social Media ausleben zu können?</p>
+                            <p className="slim-text">Wir zeigen dir einige Plattformen und Tipps, die du unbedingt ausprobieren solltest!</p>
                             <div className="button-wrapper-vert">
                                 <Button href="#tipps" className="helper-btn">Tipps</Button>
                                 <Button href="#plattformen" className="helper-btn">Features & Plattformen</Button>
@@ -90,31 +92,38 @@ class Recommendations extends React.Component {
                             <p className="info-box">Features</p>
                         </div>
                         <div id="social-media-wrapper">
-                            <div id="comparison" className="image-container-wrapper">
+                            <div id="plattformen" className="image-container-wrapper">
                                 <div className="image-container">
                                     <div>
                                         <img className="image-icon" src={facebook}/>
                                     </div>
                                     <h3 className="txt-highlight">Facebook SocialGroups</h3>
-                                    <ul className="slim-list">
-                                        <li>News & Nachrichten</li>
-                                        <li>Memes</li>
-                                        <li>Kontroverse Inhalte</li>
-                                        <li>Meinungen teilen</li>
-                                    </ul>
                                 </div>
+                                <div className="img-container">
+                                    <img className="small-img" src={socialgroups}/>
+                                </div>
+                                <ul className="slim-list">
+                                    <li>Meetups erstellen & neue Leute treffen</li>
+                                    <li>Mehr Konversation über aktuelle Themen</li>
+                                </ul>
+                                <p className="centered slim-text">Eine SocialGroup ist eine Gruppe wie jede andere, jedoch mit extra Anforderungen und Regeln, die das Sozialisieren in der
+                                    echten Welt vereinfachen sollen. Angeknüpft an die Facebook Events kann hier jeder “verifizierte” User ein gruppeninternes Event zum Treffen im
+                                    realen Leben einstellen.</p>
                                 <div className="image-container">
                                     <div>
                                         <img className="image-icon" src={youtube}/>
                                     </div>
                                     <h3 className="txt-highlight">YouTube Newcomer Feature</h3>
-                                    <ul className="slim-list">
-                                        <li>News & Nachrichten</li>
-                                        <li>Memes</li>
-                                        <li>Kontroverse Inhalte</li>
-                                        <li>Meinungen teilen</li>
-                                    </ul>
                                 </div>
+                                <div className="img-container">
+                                    <img className="small-img" src={newcomer}/>
+                                </div>
+                                <ul className="slim-list">
+                                    <li>Neue, kreative Inhalt entdecken</li>
+                                    <li>Andere Meinungen und Blickwinkel kennenlernen</li>
+                                </ul>
+                                <p className="centered slim-text">Die Newcomer Funktion erlaubt es ungefiltert und nach Beliebtheit ausgewählte Videos auf YouTube zu entdecken.
+                                Dabei werden kleine oder mittelgroße YouTuber hervorgehoben, um auch ihnen die Chance zu geben ein erfolgreicher Creator zu werden.</p>
                             </div>
                         </div>
                         <div className="icon-wrapper-big">
@@ -127,36 +136,27 @@ class Recommendations extends React.Component {
                                         <img className="image-icon" src={mightynetworks}/>
                                     </div>
                                     <h3 className="txt-highlight">MightyNetworks</h3>
-                                    <ul className="slim-list">
-                                        <li>News & Nachrichten</li>
-                                        <li>Memes</li>
-                                        <li>Kontroverse Inhalte</li>
-                                        <li>Meinungen teilen</li>
-                                    </ul>
+                                    <p className="slim-text">Ein Registrieren bei MightyNetworks ist besonders für Personen sinnvoll, die sich mit spezifischen
+                                    Hobbies oder Berufen beschäftigen. Hier gibt es verschiedene kleine und hilfsbreite Communities, die auch lokal sehr stark
+                                    vertreten sind.</p>
                                 </div>
                                 <div className="image-container">
                                     <div>
                                         <img className="image-icon" src={meetup}/>
                                     </div>
                                     <h3 className="txt-highlight">Meetup</h3>
-                                    <ul className="slim-list">
-                                        <li>News & Nachrichten</li>
-                                        <li>Memes</li>
-                                        <li>Kontroverse Inhalte</li>
-                                        <li>Meinungen teilen</li>
-                                    </ul>
+                                    <p className="slim-text">Ein Registrieren bei Meetup ist besonders für ehrgeizige und enthusiastische Personen ratsam. Für nahezu
+                                    jede Profession gibt es ein Meetup, welches Menschen mit ähnlichen Interessen im echten Leben zusammenbringt. Außerdem gibt es online
+                                    die Möglichkeit sich über die Themen auszutauschen oder digitale Inhalte zu teilen.</p>
                                 </div>
                                 <div className="image-container">
                                     <div>
                                         <img className="image-icon" src={localpost}/>
                                     </div>
                                     <h3 className="txt-highlight">local post</h3>
-                                    <ul className="slim-list">
-                                        <li>News & Nachrichten</li>
-                                        <li>Memes</li>
-                                        <li>Kontroverse Inhalte</li>
-                                        <li>Meinungen teilen</li>
-                                    </ul>
+                                    <p className="slim-text">Ein Registrieren bei local post ist besonders für Junge und heimatverwurzelte Menschen empfehlenswert.
+                                    Hier werden aktuelle, lokale Themen diskutiert und Gedanken geteilt. Die Sichtbarkeit ist dabei immer auf einen bestimmten Radius begrenzt,
+                                    um Städte oder Gemeiden näher aneinander zu bringen.</p>
                                 </div>
                             </div>
                         </div>
